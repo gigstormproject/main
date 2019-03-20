28743da84ca1a968f17cc46c59dab05d33666c21
Nils Holgerson <joseph.murgescu@stud.fh-campuswien.ac.at>
2019-03-20 17:45:54 +0100

    added doors in index.html

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+$('.doorRight').click(function(){
+    $('.doorRight').toggleClass('doorOpenLeft');
+  });
+  
+  $('.doorLeft').click(function(){
+      $('.doorLeft').toggleClass('doorOpenRight');
+    });
