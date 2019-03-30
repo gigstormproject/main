a281059b1fcd56b530f640dc7170abe975178b59
Michael Prammer <michael.prammer@stud.fh-campuswien.ac.at>
2019-03-30 15:31:51 +0100

    CSS_LogIn: Layout

--- CSS_LogIn/readContacts.inc.php
+++ CSS_LogIn/readContacts.inc.php
@@ -7,41 +7,42 @@
     // DEFINE CALLBACK FUNCTION
     con.onreadystatechange=function()
     {
-    if (con.readyState==4 && con.status==200)
-    {
-        // EXTRACT VALUE FOR HTML HEADER
-        let response=this.responseText;
-        let contacts=JSON.parse(this.responseText);
-        let col=[];
-        for (let key in contacts[0])
+        if (con.readyState==4 && con.status==200)
         {
-            col.push(key);
-        }
-        // CREATE DYNAMIC TABLE
-        let table=document.createElement("table");
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE
-        let tr=table.insertRow(-1); // TABLE ROW AT THE END
-        for (let i=0; i<col.length; i++)
-        {
-            let th=document.createElement("th"); // TABLE HEADER
-            th.innerHTML=col[i];
-            tr.appendChild(th);
-        }
-        // ADD JSON DATA TO THE TABLE AS ROWS
-        for (let i=0; i<contacts.length; i++)
-        {
-            tr=table.insertRow(-1);
-            for (let j=0; j<col.length; j++)
+            // EXTRACT VALUE FOR HTML HEADER
+            let response=this.responseText;
+            let contacts=JSON.parse(this.responseText);
+            let col=[];
+            for (let key in contacts[0])
             {
-                let tabCell=tr.insertCell(-1);
-                tabCell.innerHTML=contacts[i][col[j]];
+                col.push(key);
             }
+            // CREATE DYNAMIC TABLE
+            let table=document.createElement("table");
+            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE
+            let tr=table.insertRow(-1); // TABLE ROW AT THE END
+            for (let i=0; i<col.length; i++)
+            {
+                let th=document.createElement("th"); // TABLE HEADER
+                th.innerHTML=col[i];
+                tr.appendChild(th);
+            }
+            // ADD JSON DATA TO THE TABLE AS ROWS
+            for (let i=0; i<contacts.length; i++)
+            {
+                tr=table.insertRow(-1);
+                for (let j=0; j<col.length; j++)
+                {
+                    let tabCell=tr.insertCell(-1);
+                    tabCell.innerHTML=contacts[i][col[j]];
+                }
+            }
+            // ADD NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER
+            let divContainer=document.getElementById("showTable");
+            //divContainer.innerHTML="";
+            divContainer.appendChild(table);
         }
-        // ADD NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER
-        let divContainer=document.getElementById("showTable");
-        //divContainer.innerHTML="";
-        divContainer.appendChild(table);
-    }}
+    }
     con.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // set appropriate request header
     con.send(); // request file content
  </script>
