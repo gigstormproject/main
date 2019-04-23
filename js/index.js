ddfdc7d90bb1b1e0902aaa5016f199d10966ad13
Nils Holgerson <joseph.murgescu@stud.fh-campuswien.ac.at>
2019-04-24 00:28:38 +0200

    search can now be initiated by pressing enter inside the search field

--- js/index.js
+++ js/index.js
@@ -16,3 +16,27 @@ btn.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+// search bar function - stores Input in Web Storage
+
+function goSearch() {
+  
+  if (typeof Storage !== "undefined") {
+    // Store
+    localStorage.setItem("band", document.getElementById("band").value);
+  } else {
+    // hier Fehlermeldungs-Popup?
+    document.getElementById("result").innerHTML =
+      "Sorry, your browser does not support Web Storage...";
+  }
+  window.location.href="band_profil.html";
+}
+
+
+var input = document.getElementById("band");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("bandSearch").click();
+  }
+});
