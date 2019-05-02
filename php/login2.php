5dc2c867f7e34f373cf1e0636ad2f6e467837a60
DESKTOP-1JKN9F0\tothm <mate.toth@stud.fh-campuswien.ac.at>
2019-05-03 01:47:24 +0200

    success page+etc

--- php/login2.php
+++ php/login2.php
@@ -4,7 +4,7 @@ include 'pdo_connect.php';
 
 if(isset($_POST['uname']))
 {
-    $query = "SELECT `upassword` FROM `band` WHERE `uname` = ?";
+    $query = "SELECT `upassword` FROM `bar` WHERE `uname` = ?";
     $params = array($_POST['uname']);
     $results = dataQuery($query, $params);
 }
@@ -14,18 +14,11 @@ $hash = $results[0]['upassword']; // first and only row if username exists;
 $loggedIn = password_verify($_POST['upassword'], $hash);
 
 if($loggedIn){
-    session_start();
-    $_SESSION["username"] = $_POST['uname'];
-    $_SESSION["bandname"] = $_POST['bname'];
-    setcookie("band", $_POST['uname'], time() + (86400 * 30), "/");
-    echo "
-    <script>
-    window.location.href='/pages/band_profil.html';
+    echo "  
+    <script> alert('Success');
     </script>
     ";
-
-    }
-    else {
+ }else{
     echo "  
     <script> alert('Wrong Username/Password');
     window.location = '../pages/index.html';
--- php/register_band.php
+++ php/register_band.php
@@ -4,17 +4,21 @@ include 'pdo_connect.php';
 
 if(isset($_POST['submit']))
 {
-    $bname = $_POST['bname'];
     $uname = $_POST['uname'];
     $email = $_POST['email'];
     $genre = $_POST['genre'];
     $upassword = password_hash($_POST['upassword'], PASSWORD_DEFAULT);
 
-    $query = 'INSERT INTO `band` (`bname`, `uname`, `email`, `genre`, `upassword`) VALUES (?,?,?,?,?)';
-    $params = array($bname, $uname, $email, $genre, $upassword);
+    $query = 'INSERT INTO `band` (`uname`, `email`, `genre`, `upassword`) VALUES (?,?,?,?)';
+    $params = array($uname, $email, $genre, $upassword);
     $results = dataQuery($query, $params);
 
     echo 1 == $results ? 'success' : 'failure';
+
+    if($results==1){
+    echo "<script>window.location = '../pages/register_success.html';
+    </script>";
+    }
 }
 
 ?>
