63740e29e19fc00b2b4a641ac77fc0535217bb0e
Michael Prammer <michael.prammer@stud.fh-campuswien.ac.at>
2019-05-09 18:54:29 +0200

    DATENBANK: - Table angepasst (bname bei Band wurde nicht verwendet, daher gelöscht); - je drei User registriert in Band und Bar

--- db_gigstorm/db_gigstorm.sql
+++ db_gigstorm/db_gigstorm.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2019. Ápr 24. 19:50
--- Kiszolgáló verziója: 10.1.38-MariaDB
--- PHP verzió: 7.3.2
+-- Host: 127.0.0.1
+-- Erstellungszeit: 09. Mai 2019 um 18:51
+-- Server-Version: 10.1.38-MariaDB
+-- PHP-Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,36 +19,53 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `db_gigstorm`
+-- Datenbank: `db_gigstorm`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `band`
+-- Tabellenstruktur für Tabelle `band`
 --
 
 CREATE TABLE `band` (
-  `bname` varchar(255) DEFAULT NULL,
-  `uname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `genre` varchar(255) NOT NULL,
-  `upassword` varchar(255) NOT NULL
+  `uname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `upassword` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `band`
+--
+
+INSERT INTO `band` (`uname`, `email`, `genre`, `upassword`) VALUES
+('Muse', 'muse@muse.com', 'Rock', '$2y$10$HlkkFM2LY.2JCZRN3HKUNu4a0Zy/i6ak2HXn6biC4Bh9keXx4EOrq'),
+('The Dresden Dolls', 'tdd@dolls.com', 'Alternative', '$2y$10$PIH2mVWrFwqioJLC3eTeRudPMgn4xfAGGVYlnFqqetdFXEzr/EPC.'),
+('Rammstein', 'ramm@stein.de', 'Other', '$2y$10$Z9tNBlDw4uK/KQuF4jEAp.D1Z.Yz6.axwAe9djz/GiZFM0jGmPe5q');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `bar`
+-- Tabellenstruktur für Tabelle `bar`
 --
 
 CREATE TABLE `bar` (
-  `fullname` varchar(255) DEFAULT NULL,
-  `barname` varchar(255) DEFAULT NULL,
-  `uname` varchar(255) DEFAULT NULL,
-  `upassword` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `fullname` varchar(50) DEFAULT NULL,
+  `barname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `upassword` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `bar`
+--
+
+INSERT INTO `bar` (`fullname`, `barname`, `uname`, `upassword`, `email`) VALUES
+('Thomas Gottschalk', 'Nudelbar', 'TGott', '$2y$10$iNxfE2E1UuJ.oyaESu06ceR6AxLXQyqISrTj1zuvc104FYFCnAAVS', 't.gottschalk@aol.de'),
+('Hans Gruber', 'Sprengbar', 'HGichT', '$2y$10$EDTsZPX64ZIs/Dq3jZpub.QldK7f2FNGTUiUf8X765GRwb.L.NMxO', 'hg69@gmail.com'),
+('Bilbo Beutlin', 'Auenland', 'BB', '$2y$10$hlmdf1MfbT3jODCyUM5F0OrCp1crdMb0EKY8Q8AqQ6ZJvsjNgHz0.', 'bilbo@lotr.com');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
